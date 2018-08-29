763e41c94350d363dbd27524ce045e6b0dabd989
Paulo Lima <pflima92@gmail.com>
2018-08-28 23:17:24 -0300

    Adding Swagger

--- src/main/java/io/github/blueprint/controller/PersonController.java
+++ src/main/java/io/github/blueprint/controller/PersonController.java
@@ -1,6 +1,8 @@
 package io.github.blueprint.controller;
 
 import io.github.blueprint.model.Person;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("The Person Resource Rest Api")
 @RestController
 public class PersonController {
 
   private static Set<Person> persons = new HashSet<>();
 
+  @ApiOperation("Save a new person")
   @PostMapping("/persons")
   public void savePerson(@RequestBody Person person) {
     persons.add(person);
   }
 
+  @ApiOperation("List all persons available")
   @GetMapping("/persons")
   public Collection<Person> listPersons() {
     return persons;
